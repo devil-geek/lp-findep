00a0302fd1bb8d14cdd29a5927727578da5a6f99
devil-geek <devil.prowler@gmail.com>
2019-02-17 03:19:25 -0600

    Added metatags and meta tag change with title change

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Element } from 'react-scroll'
 import queryString from 'query-string'
+import MetaTags from 'react-meta-tags';
 import Banner from "../../components/Banner/Banner";
 import About from "../../components/About/About";
 import Requirements from "../../components/Requirements/Requirements";
@@ -13,12 +14,17 @@ import Slider from "../../components/Slider/Slider";
 
 const Home = (props) => {
   let title = queryString.parse(props.location.search).t
+  let content = "Préstamos personales, tu tienes el plan, nosotros el préstamo que necesitas. En financiera independencia te ofrecemos créditos sin aval, sin garantías, fácil, rápido y sin tanto rollo."
   if(title){
     title = title.replace(/[|&;$%@"<>()_·+,]/g, " ")
     title = title.charAt(0).toUpperCase() + title.slice(1)
+    content = title + ", tu tienes el plan, nosotros el préstamo que necesitas. En financiera independencia te ofrecemos créditos sin aval, sin garantías, fácil, rápido y sin tanto rollo."
   }
   return (
     <div>
+      <MetaTags>
+        <meta name="description" content={content} />
+      </MetaTags >
         <Element name="main">
         <div>
           <Banner title={title}/>
